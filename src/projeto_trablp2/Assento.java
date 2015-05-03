141c3a690960bc7202ef0f60b98912f2a7ef797e
Isaac <yzaak.silva@gmail.com>
2015-05-03 14:16:34 -0300

    Assento

---
+++ src/projeto_trablp2/Assento.java
@@ -0,0 +1,13 @@
+package projeto_trablp2;
+
+/**
+ * @author André Sobreira
+ * tipo do assento(classe), Controle de assentos(disponivel ou vendido)
+ */
+public class Assento {
+    private boolean status;
+    
+    public Assento(){
+        status=false;
+    }
+}
